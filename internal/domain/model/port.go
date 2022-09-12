ef3d440314ae0ad524714f19d3e21b12a2343392
Michal Grzybek <michal.grzybek@outlook.com>
2022-09-12 16:28:59 +0000

    Fix json anotations (switch from string to json.Number)

--- internal/domain/model/port.go
+++ internal/domain/model/port.go
@@ -1,15 +1,17 @@
 package model
 
+import "encoding/json"
+
 type Seaport struct {
-	Id          string   `json:"id,omitempty"`
-	Name        string   `json:"name,omitempty"`
-	City        string   `json:"city,omitempty"`
-	Country     string   `json:"country,omitempty"`
-	Alias       []string `json:"alias,omitempty"`
-	Regions     []string `json:"regions,omitempty"`
-	Coordinates []string `json:"coordinates,omitempty"`
-	Province    string   `json:"province,omitempty"`
-	Timezone    string   `json:"timezone,omitempty"`
-	Unlocs      []string `json:"unlocs,omitempty"`
-	Code        string   `json:"code,omitempty"`
+	Id          string        `json:"id,omitempty"`
+	Name        string        `json:"name,omitempty"`
+	City        string        `json:"city,omitempty"`
+	Country     string        `json:"country,omitempty"`
+	Alias       []string      `json:"alias,omitempty"`
+	Regions     []string      `json:"regions,omitempty"`
+	Coordinates []json.Number `json:"coordinates,omitempty"`
+	Province    string        `json:"province,omitempty"`
+	Timezone    string        `json:"timezone,omitempty"`
+	Unlocs      []string      `json:"unlocs,omitempty"`
+	Code        string        `json:"code,omitempty"`
 }
